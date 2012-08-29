80203553f6c1880c0e32c1b9b0f2e9542dc56c2c
Nhorning <ictsupport@pbi-nepal.org>
2012-08-29 18:50:27 +0545

    Update views/facebook-social.php
    Google based research indicated that this worked better. It fixed the runtime error I was getting without breaking functionality.

--- views/facebook-social.php
+++ views/facebook-social.php
@@ -1,5 +1,5 @@
 <div class="content">
 
-<div id="fb-root"></div><script src="//connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:comments href="<?php echo url::base().trim($_SERVER['REDIRECT_URL'],'/'); ?>" num_posts="2" width="530"></fb:comments>
+<div id="fb-root"></div><script src="//connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:comments href="<?php echo url::base().trim($_SERVER['REQUEST_URI'],'/'); ?>" num_posts="2" width="530"></fb:comments>
 
 </div>
